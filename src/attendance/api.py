3ccd703f97edaa78ac7b049dacb41b74d38f184c
monil sojitra <monilsojitra.work@gmail.com>
2025-10-13 12:52:34 +0530

    update attendance API

--- src/attendance/api.py
+++ src/attendance/api.py
@@ -6,13 +6,14 @@ from src.user.utils.deps import is_authorized_for
 from src.attendance.crud import attendance_crud
 from src.attendance.enums import AttendanceEnum
 from src.attendance.schemas import AttendanceBaseSchema, AttendanceRequestSchema
+from utils.db.session import get_db
 
 attendance_router = APIRouter()
 
 @attendance_router.post("/attendance", status_code=status.HTTP_201_CREATED, response_model=AttendanceBaseSchema)
-def create_attendance(request: AttendanceRequestSchema, user_db: is_authorized_for([UserRoles.STUDENT.value, UserRoles.SUPER_ADMIN.value])):
+def create_attendance(request: AttendanceRequestSchema, db: get_db):
     try:
-        user, db = user_db
+        # user, db = user_db
         
         is_exist = attendance_crud.get_by_user_and_qr_id(db=db, student_id=request.student_id, qr_code_id=request.qr_code_id)
         if is_exist:
